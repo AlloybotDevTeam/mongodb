be4b5b8b62c8b867f9073f95d732401d99d09df3
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-11-25 09:52:04 -0700

    Working

--- MongoDB.ts
+++ MongoDB.ts
@@ -1,16 +1,15 @@
-import { IConnection, Alloybot } from '../../../Alloybot';
+import { IConnection, Alloybot, DependantList } from '../../../Alloybot';
+import { MongoClient } from 'mongodb';
 
 export default class MongoDB implements IConnection {
-  public readonly name: String = 'MongoDB';
-  public readonly dependencies: String[] = [];
-  public connection: any = {};
+  public readonly name: string = 'MongoDB';
+  public readonly dependencies: string[] = [];
+  public readonly dependants: DependantList = Alloybot.getDependants(this.name);
+  public connection: any;
 
-  constructor(
-    host: String = 'localhost',
-    port: Number = 27017,
-    username?: String,
-    password?: String
-  ) {}
+  constructor(uri: string) {
+    this.connection = new MongoClient(uri);
+  }
 }
 
-Alloybot.registerConnection(new MongoDB());
+Alloybot.registerConnection(new MongoDB(process.env['MONGODB.URI']));
