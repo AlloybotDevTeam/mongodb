eb6eb345df06573a0f889e6a366a1af488914a82
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-12-16 22:33:31 -0700

    LETS GET IT

--- MongoDB.ts
+++ MongoDB.ts
@@ -1,15 +1,24 @@
-import { default as Alloybot, Type, Util } from '../../Alloybot';
+import { default as Alloybot, Type, Util, ConfigBuilder } from '../../Alloybot';
 import { MongoClient } from 'mongodb';
 
 export default class MongoDB implements Type.IConnection {
   public readonly name: string = 'MongoDB';
   public readonly dependencies: string[] = [];
   public readonly dependants: Type.IPlugin[] = Alloybot.getDependants(this.name);
-  public connection: any;
+  public connection: MongoClient;
+  public config;
 
-  constructor(uri: string) {
-    this.connection = new MongoClient(uri);
+  constructor() {
+    let Config: ConfigBuilder = new ConfigBuilder('MongoDB', require('./package.json').version);
+    Config.addOption('uri', ['string'], 'Documentation Link Here!');
+    Config.close();
+    this.config = Config.getConfig();
+  }
+
+  public connect(): MongoDB {
+    this.connection = new MongoClient(this.config.uri);
+    return this;
   }
 }
 
-Alloybot.registerConnection(new MongoDB(process.env['MONGODB.URI']));
+Alloybot.registerConnection(new MongoDB().connect());
